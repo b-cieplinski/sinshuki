dff2516f252ccb75b01cc8b6d7d4b824a8a9c4fe
Brandon Cieplinski <bcieplinski0@gmail.com>
2022-05-17 10:22:19 +0200

    Product copy update

--- pages/collection/histeria.js
+++ pages/collection/histeria.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header'
 import ProductListHeader from '../../components/products/ProductListHeader'
-import ProductListVariantA from '../../components/products/ProductListVariantA'
-import ProductListVariantB from '../../components/products/ProductListVariantB'
-import ProductListVariantC from '../../components/products/ProductListVariantC'
 
 const products = () => {
   return (
     <div className="bg-rose-50">
         <Header />
         <ProductListHeader />
-        <ProductListVariantA />
-        <ProductListVariantB />
-        <ProductListVariantC />
         <div></div>
 
     </div>
